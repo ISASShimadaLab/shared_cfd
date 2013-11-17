f5c899bee3ac2091905f660bfa63a303a22c1f3e
shotayam <2921275202@mail.ecc.u-tokyo.ac.jp>
2013-11-17 11:53:24 +0900

    muscl modification

--- store/high_order/muscl/muscl.f90
+++ store/high_order/muscl/muscl.f90
@@ -6,68 +6,70 @@ module half_value
 contains
 subroutine set_HV(q,qHli,qHri,qHlj,qHrj)!van albada
    implicit none
-   double precision,intent(in)::    q(1:dimw,-1:ni+2,-1:nj+2)
-   double precision,intent(out)::qHli(1:dimw, 0:ni,   1:nj)
-   double precision,intent(out)::qHri(1:dimw, 0:ni,   1:nj)
-   double precision,intent(out)::qHlj(1:dimw, 1:ni,   0:nj)
-   double precision,intent(out)::qHrj(1:dimw, 1:ni,   0:nj)
+   double precision,intent(in)::    q(1:dimw,-1:nimax+2,-1:njmax+2,Nplane)
+   double precision,intent(out)::qHli(1:dimw, 0:nimax,   1:njmax,Nplane)
+   double precision,intent(out)::qHri(1:dimw, 0:nimax,   1:njmax,Nplane)
+   double precision,intent(out)::qHlj(1:dimw, 1:nimax,   0:njmax,Nplane)
+   double precision,intent(out)::qHrj(1:dimw, 1:nimax,   0:njmax,Nplane)
 
-   double precision          delta_ui(1:dimw,0:ni+1,  1:nj)
-   double precision          delta_li(1:dimw,0:ni+1,  1:nj)
-   double precision          delta_uj(1:dimw,  1:ni,0:nj+1)
-   double precision          delta_lj(1:dimw,  1:ni,0:nj+1)
+   double precision          delta_ui(1:dimw,0:nimax+1,1:njmax)
+   double precision          delta_li(1:dimw,0:nimax+1,1:njmax)
+   double precision          delta_uj(1:dimw,1:nimax,  0:njmax+1)
+   double precision          delta_lj(1:dimw,1:nimax,  0:njmax+1)
    double precision,dimension(dimw)::Du,Dl,s
    double precision,parameter::epsln=1d-300
    !double precision,parameter::epsln=1d-6
-   integer i,j
-   
-   !$omp parallel do default(none) private(i,Du,Dl,s) shared(j,q,delta_ui,delta_li,nxs,nxe,nys,nye,phi_mus,k_mus)
-   do j=nys,nye
-      do i=nxs-1,nxe+1
-         Du(1:dimw)=q(:,i+1,j)-q(:,i  ,j)
-         Dl(1:dimw)=q(:,i  ,j)-q(:,i-1,j)
+   integer i,j,plane
+  
+   do plane = nps,npe 
+      !$omp parallel do default(none) private(i,Du,Dl,s) shared(j,q,delta_ui,delta_li,nxs,nxe,nys,nye,phi_mus,k_mus,plane)
+      do j=nys(plane),nye(plane)
+         do i=nxs(plane)-1,nxe(plane)+1
+            Du(1:dimw)=q(:,i+1,j,plane)-q(:,i  ,j,plane)
+            Dl(1:dimw)=q(:,i  ,j,plane)-q(:,i-1,j,plane)
 
-         !s=(2d0*Du*Dl+epsln)/(Du**2+Dl**2+epsln)
-         s=(2d0*Du*Dl)/(Du**2+Dl**2+epsln)
+            !s=(2d0*Du*Dl+epsln)/(Du**2+Dl**2+epsln)
+            s=(2d0*Du*Dl)/(Du**2+Dl**2+epsln)
 
-         delta_ui(:,i,j)=0.5d0*s*((1d0+k_mus*s)*Du+(1d0-k_mus*s)*Dl)
-         delta_li(:,i,j)=0.5d0*s*((1d0+k_mus*s)*Dl+(1d0-k_mus*s)*Du)
+            delta_ui(:,i,j)=0.5d0*s*((1d0+k_mus*s)*Du+(1d0-k_mus*s)*Dl)
+            delta_li(:,i,j)=0.5d0*s*((1d0+k_mus*s)*Dl+(1d0-k_mus*s)*Du)
+         end do
       end do
-   end do
-   !$omp end parallel do
+      !$omp end parallel do
 
-   !$omp parallel do default(none) private(i) shared(j,q,delta_ui,delta_li,qHli,qHri,nxs,nxe,nys,nye,phi_mus,k_mus)
-   do j=nys,nye
-      do i=nxs-1,nxe
-         qHli(:,i,j)=q(1:dimw,i  ,j)+phi_mus*0.5d0*delta_ui(1:dimw,i  ,j)
-         qHri(:,i,j)=q(1:dimw,i+1,j)-phi_mus*0.5d0*delta_li(1:dimw,i+1,j)
+      !$omp parallel do default(none) private(i) shared(j,q,delta_ui,delta_li,qHli,qHri,nxs,nxe,nys,nye,phi_mus,k_mus,plane)
+      do j=nys(plane),nye(plane)
+         do i=nxs(plane)-1,nxe(plane)
+            qHli(:,i,j,plane)=q(1:dimw,i  ,j,plane)+phi_mus*0.5d0*delta_ui(1:dimw,i  ,j)
+            qHri(:,i,j,plane)=q(1:dimw,i+1,j,plane)-phi_mus*0.5d0*delta_li(1:dimw,i+1,j)
+         end do
       end do
-   end do
-   !$omp end parallel do
+      !$omp end parallel do
 
-   !$omp parallel do default(none) private(i,Du,Dl,s) shared(j,q,delta_uj,delta_lj,nxs,nxe,nys,nye,phi_mus,k_mus)
-   do j=nys-1,nye+1
-      do i=nxs,nxe
-         Du(1:dimw)=q(:,i,j+1)-q(:,i,j  )
-         Dl(1:dimw)=q(:,i,j  )-q(:,i,j-1)
+      !$omp parallel do default(none) private(i,Du,Dl,s) shared(j,q,delta_uj,delta_lj,nxs,nxe,nys,nye,phi_mus,k_mus,plane)
+      do j=nys(plane)-1,nye(plane)+1
+         do i=nxs(plane),nxe(plane)
+            Du(1:dimw)=q(:,i,j+1,plane)-q(:,i,j  ,plane)
+            Dl(1:dimw)=q(:,i,j  ,plane)-q(:,i,j-1,plane)
 
-         !s=(2d0*Du*Dl+epsln)/(Du**2+Dl**2+epsln)
-         s=(2d0*Du*Dl)/(Du**2+Dl**2+epsln)
+            !s=(2d0*Du*Dl+epsln)/(Du**2+Dl**2+epsln)
+            s=(2d0*Du*Dl)/(Du**2+Dl**2+epsln)
 
-         delta_uj(:,i,j)=0.5d0*s*((1d0+k_mus*s)*Du+(1d0-k_mus*s)*Dl)
-         delta_lj(:,i,j)=0.5d0*s*((1d0+k_mus*s)*Dl+(1d0-k_mus*s)*Du)
+            delta_uj(:,i,j)=0.5d0*s*((1d0+k_mus*s)*Du+(1d0-k_mus*s)*Dl)
+            delta_lj(:,i,j)=0.5d0*s*((1d0+k_mus*s)*Dl+(1d0-k_mus*s)*Du)
+         end do
       end do
-   end do
-   !$omp end parallel do
+      !$omp end parallel do
 
-   !$omp parallel do default(none) private(i) shared(j,q,delta_uj,delta_lj,qHlj,qHrj,nxs,nxe,nys,nye,phi_mus,k_mus)
-   do j=nys-1,nye
-      do i=nxs,nxe
-         qHlj(:,i,j)=q(1:dimw,i,j  )+phi_mus*0.5d0*delta_uj(1:dimw,i,j  )
-         qHrj(:,i,j)=q(1:dimw,i,j+1)-phi_mus*0.5d0*delta_lj(1:dimw,i,j+1)
+      !$omp parallel do default(none) private(i) shared(j,q,delta_uj,delta_lj,qHlj,qHrj,nxs,nxe,nys,nye,phi_mus,k_mus,plane)
+      do j=nys(plane)-1,nye(plane)
+         do i=nxs(plane),nxe(plane)
+            qHlj(:,i,j,plane)=q(1:dimw,i,j  ,plane)+phi_mus*0.5d0*delta_uj(1:dimw,i,j  )
+            qHrj(:,i,j,plane)=q(1:dimw,i,j+1,plane)-phi_mus*0.5d0*delta_lj(1:dimw,i,j+1)
+         end do
       end do
+      !$omp end parallel do
    end do
-   !$omp end parallel do
 end subroutine set_HV
 end module half_value
 
