118b3223f6f093af743ce0bb34b737734f146e35
shotayam <2921275202@mail.ecc.u-tokyo.ac.jp>
2013-11-17 13:11:56 +0900

    sample/no03 and modification of LU-SGS done

--- store/time_scheme/LU-SGS/time_lusgs_viscous.f90
+++ store/time_scheme/LU-SGS/time_lusgs_viscous.f90
@@ -3,60 +3,62 @@ subroutine calc_next_step_implicit
    use variable
    use var_lusgs
    implicit none
-   integer i,j,sm
-   double precision Dq(1:dimq,nxs-1:nxe+1,nys-1:nye+1)
+   integer i,j,sm,plane
+   double precision Dq(1:dimq,0:nimax+1,0:njmax+1,Nplane)
 
-   !set RHS
-   !$omp parallel do private(i)
-   do j=nys,nye
-      do i=nxs,nxe
-         Dq(:,i,j)= dsi(i-1,  j)*(TGi(1:dimq,i-1,j  )-TGvi(1:dimq,i-1,j  ))&
-                   -dsi(  i,  j)*(TGi(1:dimq,i  ,j  )-TGvi(1:dimq,i  ,j  ))&
-                   +dsj(  i,j-1)*(TGj(1:dimq,i  ,j-1)-TGvj(1:dimq,i  ,j-1))&
-                   -dsj(  i,  j)*(TGj(1:dimq,i  ,j  )-TGvj(1:dimq,i  ,j  ))
+   do plane = nps,npe
+      !set RHS
+      !$omp parallel do private(i)
+      do j=nys(plane),nye(plane)
+         do i=nxs(plane),nxe(plane)
+            Dq(:,i,j,plane)= dsi(i-1,  j,plane)*(TGi(1:dimq,i-1,j  ,plane)-TGvi(1:dimq,i-1,j  ,plane))&
+                            -dsi(  i,  j,plane)*(TGi(1:dimq,i  ,j  ,plane)-TGvi(1:dimq,i  ,j  ,plane))&
+                            +dsj(  i,j-1,plane)*(TGj(1:dimq,i  ,j-1,plane)-TGvj(1:dimq,i  ,j-1,plane))&
+                            -dsj(  i,  j,plane)*(TGj(1:dimq,i  ,j  ,plane)-TGvj(1:dimq,i  ,j  ,plane))
+         end do
       end do
-   end do
-   !$omp end parallel do
+      !$omp end parallel do
 
-   !set BC Delta q
-   Dq(:,nxs-1,:)=0d0
-   Dq(:,:,nys-1)=0d0
+      !set BC Delta q
+      Dq(:,nxs(plane)-1,nys(plane)-1:nye(plane),plane)=0d0
+      Dq(:,nxs(plane)-1:nxe(plane),nys(plane)-1,plane)=0d0
 
-   !calculate forward
-   do sm=nxs+nys,nxe+nye
-      !$omp parallel do private(j)
-      do i=max(nxs,sm-nye),min(sm-nys,nxe)
-         j=sm-i
-         Dq(:,i,j)=alpha(i,j)*(Dq(:,i,j)&
-                              +dsci(i-1,j  )*matmul(Ap(:,:,i-1,j  ),Dq(:,i-1,j  ))&
-                              +dscj(i  ,j-1)*matmul(Bp(:,:,i  ,j-1),Dq(:,i  ,j-1)))
+      !calculate forward
+      do sm=nxs(plane)+nys(plane),nxe(plane)+nye(plane)
+         !$omp parallel do private(j)
+         do i=max(nxs(plane),sm-nye(plane)),min(sm-nys(plane),nxe(plane))
+            j=sm-i
+            Dq(:,i,j,plane)=alpha(i,j,plane)*(Dq(:,i,j,plane)&
+                                             +dsci(i-1,j  ,plane)*matmul(Ap(:,:,i-1,j  ,plane),Dq(:,i-1,j  ,plane))&
+                                             +dscj(i  ,j-1,plane)*matmul(Bp(:,:,i  ,j-1,plane),Dq(:,i  ,j-1,plane)))
+         end do
+         !$omp end parallel do
+      end do
+
+      !set BC Delta q
+      Dq(:,nxe(plane)+1,nys(plane):nye(plane)+1,plane)=0d0
+      Dq(:,nxs(plane):nxe(plane)+1,nye(plane)+1,plane)=0d0
+
+      !calculate backward
+      do sm=nxe(plane)+nye(plane),nxs(plane)+nys(plane),-1
+         !$omp parallel do private(j)
+         do i=max(nxs(plane),sm-nye(plane)),min(sm-nys(plane),nxe(plane))
+            j=sm-i
+            Dq(:,i,j,plane)=Dq(:,i,j,plane)&
+                           -alpha(i,j,plane)*(dsci(i+1,j  ,plane)*matmul(Am(:,:,i+1,j  ,plane),Dq(:,i+1,j  ,plane))&
+                                             +dscj(i  ,j+1,plane)*matmul(Bm(:,:,i  ,j+1,plane),Dq(:,i  ,j+1,plane)))
+         end do
+         !$omp end parallel do
+      end do
+
+      !$omp parallel do private(i)
+      !add Dq
+      do j=nys(plane),nye(plane)
+         do i=nxs(plane),nxe(plane)
+            q(:,i,j,plane)=q(:,i,j,plane)+Dq(:,i,j,plane)
+         end do
       end do
       !$omp end parallel do
    end do
-
-   !set BC Delta q
-   Dq(:,nxe+1,:)=0d0
-   Dq(:,:,nye+1)=0d0
-
-   !calculate backward
-   do sm=nxe+nye,nxs+nys,-1
-      !$omp parallel do private(j)
-      do i=max(nxs,sm-nye),min(sm-nys,nxe)
-         j=sm-i
-         Dq(:,i,j)=Dq(:,i,j)&
-                  -alpha(i,j)*(dsci(i+1,j  )*matmul(Am(:,:,i+1,j  ),Dq(:,i+1,j  ))&
-                              +dscj(i  ,j+1)*matmul(Bm(:,:,i  ,j+1),Dq(:,i  ,j+1)))
-      end do
-      !$omp end parallel do
-   end do
-
-   !$omp parallel do private(i)
-   !add Dq
-   do j=nys,nye
-      do i=nxs,nxe
-         q(:,i,j)=q(:,i,j)+Dq(:,i,j)
-      end do
-   end do
-   !$omp end parallel do
 end subroutine calc_next_step_implicit
 
