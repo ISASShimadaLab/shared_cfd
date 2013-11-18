eb23e32d6ee7f1e94ac69bdeed215456e1c5d961
shotayam <2921275202@mail.ecc.u-tokyo.ac.jp>
2013-11-18 14:25:06 +0900

    precon update

--- store/time_scheme/dual/time_dual_viscous.f90
+++ store/time_scheme/dual/time_dual_viscous.f90
@@ -5,90 +5,93 @@ subroutine calc_next_step_implicit(step_internal)
    use var_lusgs
    implicit none
    integer,intent(in)::step_internal
-   integer i,j,k,sm
-   double precision Dq(1:dimq,nxs-1:nxe+1,nys-1:nye+1)
+   integer i,j,k,sm,plane
+   double precision Dq(1:dimq,0:nimax+1,0:njmax+1,Nplane)
+   double precision Dq_small(1:dimq)
    double precision omega,tmp
-
-   !set RHS
-   !$omp parallel do private(i)
-   do j=nys,nye
-      do i=nxs,nxe
-         Dq(:,i,j)= dsi(i-1,  j)*(TGi(1:dimq,i-1,j  )-TGvi(1:dimq,i-1,j  ))&
-                   -dsi(  i,  j)*(TGi(1:dimq,i  ,j  )-TGvi(1:dimq,i  ,j  ))&
-                   +dsj(  i,j-1)*(TGj(1:dimq,i  ,j-1)-TGvj(1:dimq,i  ,j-1))&
-                   -dsj(  i,  j)*(TGj(1:dimq,i  ,j  )-TGvj(1:dimq,i  ,j  ))
-         Dq(:,i,j)= Dq(:,i,j)-Vol(i,j)*dqdt(:,i,j)
-
-         Dq(:,i,j)=Dq(:,i,j)+pre_sca(2,i,j)*dot_product(pre2(:,i,j),Dq(:,i,j))*pre1(:,i,j)
-      end do
-   end do
-   !$omp end parallel do
-
-   !!!!!!!!!!!!! FIRST STEP !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-   !set BC Delta q
-   Dq(:,nxs-1,:)=0d0
-   Dq(:,:,nys-1)=0d0
-
-   !calculate forward
-   do sm=nxs+nys,nxe+nye
-      !$omp parallel do private(j)
-      do i=max(nxs,sm-nye),min(sm-nys,nxe)
-         j=sm-i
-         Dq(:,i,j)=alpha(i,j)*(Dq(:,i,j)&
-                              +dsci(i-1,j  )*matmul(Ap(:,:,i-1,j  ),Dq(:,i-1,j  ))&
-                              +dscj(i  ,j-1)*matmul(Bp(:,:,i  ,j-1),Dq(:,i  ,j-1)))
-
-         Dq(:,i,j)=Dq(:,i,j)+pre_sca(4,i,j) *dot_product(pre2(:,i,j),Dq(:,i,j)) *pre1(:,i,j)
-         Dq(:,i,j)=1d0/pre_sca(1,i,j)*Dq(:,i,j)
-      end do
-      !$omp end parallel do
-   end do
-
-   !!!!!!!!!!!!! SECOND STEP !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-   !$omp parallel do private(i)
-   do j=nys,nye
-      do i=nxs,nxe
-         Dq(:,i,j)=Dq(:,i,j)+pre_sca(3,i,j) *dot_product(pre2(:,i,j),Dq(:,i,j)) *pre1(:,i,j)
-         Dq(:,i,j)=pre_sca(1,i,j)*Dq(:,i,j)
-      end do
-   end do
-   !$omp end parallel do
-
-   !!!!!!!!!!!!! THIRD STEP !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-   !set BC Delta q
-   Dq(:,nxe+1,:)=0d0
-   Dq(:,:,nye+1)=0d0
 
    omega=1d300
    res = 0d0
-   !calculate backward
-   do sm=nxe+nye,nxs+nys,-1
-      !$omp parallel do private(j,k) reduction(min:omega) reduction(+:res)
-      do i=max(nxs,sm-nye),min(sm-nys,nxe)
-         j=sm-i
-         Dq(:,i,j)=Dq(:,i,j)&
-                  -alpha(i,j)*(dsci(i+1,j  )*matmul(Am(:,:,i+1,j  ),Dq(:,i+1,j  ))&
-                              +dscj(i  ,j+1)*matmul(Bm(:,:,i  ,j+1),Dq(:,i  ,j+1)))
+   do plane = nps,npe
+      !set RHS
+      !$omp parallel do private(i,Dq_small)
+      do j=nys(plane),nye(plane)
+         do i=nxs(plane),nxe(plane)
+            Dq_small= dsi(i-1,  j,plane)*(TGi(1:dimq,i-1,j  ,plane)-TGvi(1:dimq,i-1,j  ,plane))&
+                     -dsi(  i,  j,plane)*(TGi(1:dimq,i  ,j  ,plane)-TGvi(1:dimq,i  ,j  ,plane))&
+                     +dsj(  i,j-1,plane)*(TGj(1:dimq,i  ,j-1,plane)-TGvj(1:dimq,i  ,j-1,plane))&
+                     -dsj(  i,  j,plane)*(TGj(1:dimq,i  ,j  ,plane)-TGvj(1:dimq,i  ,j  ,plane))
+            Dq(:,i,j,plane)= Dq_small-Vol(i,j,plane)*dqdt(:,i,j,plane)
 
-         Dq(:,i,j)=Dq(:,i,j)+pre_sca(4,i,j) *dot_product(pre2(:,i,j),Dq(:,i,j)) *pre1(:,i,j)
-         Dq(:,i,j)=1d0/pre_sca(1,i,j)*Dq(:,i,j)
-
-         res =res +Dq(nY+3,i,j)**2
-         !do k=1,dimq
-         !   res =res +Dq(k,i,j)**2
-         !end do
-
-         do k=1,nY
-            if(Dq(k,i,j)<0d0) then
-               omega = min(omega,-q(k,i,j)/Dq(k,i,j)*Dqmax)
-               if(omega<omega_min) then
-                  print *,"Error: Omega becomes too small. omega = ",omega
-                  call exit(1)
-               end if
-            end if
+            Dq(:,i,j,plane)=Dq(:,i,j,plane)+pre_sca(2,i,j,plane)*dot_product(pre2(:,i,j,plane),Dq(:,i,j,plane))*pre1(:,i,j,plane)
          end do
       end do
       !$omp end parallel do
+
+      !!!!!!!!!!!!! FIRST STEP !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !set BC Delta q
+      Dq(:,nxs(plane)-1,nys(plane)-1:nye(plane),plane)=0d0
+      Dq(:,nxs(plane)-1:nxe(plane),nys(plane)-1,plane)=0d0
+
+      !calculate forward
+      do sm=nxs(plane)+nys(plane),nxe(plane)+nye(plane)
+         !$omp parallel do private(j,Dq_small)
+         do i=max(nxs(plane),sm-nye(plane)),min(sm-nys(plane),nxe(plane))
+            j=sm-i
+            Dq_small = dsci(i-1,j  ,plane)*matmul(Ap(:,:,i-1,j  ,plane),Dq(:,i-1,j  ,plane))&
+                      +dscj(i  ,j-1,plane)*matmul(Bp(:,:,i  ,j-1,plane),Dq(:,i  ,j-1,plane))
+            Dq(:,i,j,plane)=alpha(i,j,plane)*(Dq(:,i,j,plane)+Dq_small)
+
+            Dq(:,i,j,plane)=Dq(:,i,j,plane)+pre_sca(4,i,j,plane) *dot_product(pre2(:,i,j,plane),Dq(:,i,j,plane)) *pre1(:,i,j,plane)
+            Dq(:,i,j,plane)=1d0/pre_sca(1,i,j,plane)*Dq(:,i,j,plane)
+         end do
+         !$omp end parallel do
+      end do
+
+      !!!!!!!!!!!!! SECOND STEP !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !$omp parallel do private(i)
+      do j=nys(plane),nye(plane)
+         do i=nxs(plane),nxe(plane)
+            Dq(:,i,j,plane)=Dq(:,i,j,plane)+pre_sca(3,i,j,plane) *dot_product(pre2(:,i,j,plane),Dq(:,i,j,plane)) *pre1(:,i,j,plane)
+            Dq(:,i,j,plane)=pre_sca(1,i,j,plane)*Dq(:,i,j,plane)
+         end do
+      end do
+      !$omp end parallel do
+
+      !!!!!!!!!!!!! THIRD STEP !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !set BC Delta q
+      Dq(:,nxe(plane)+1,nys(plane):nye(plane)+1,plane)=0d0
+      Dq(:,nxs(plane):nxe(plane)+1,nye(plane)+1,plane)=0d0
+
+      !calculate backward
+      do sm=nxe(plane)+nye(plane),nxs(plane)+nys(plane),-1
+         !$omp parallel do private(j,k) reduction(min:omega) reduction(+:res)
+         do i=max(nxs(plane),sm-nye(plane)),min(sm-nys(plane),nxe(plane))
+            j=sm-i
+            Dq(:,i,j,plane)=Dq(:,i,j,plane)&
+                           -alpha(i,j,plane)*(dsci(i+1,j  ,plane)*matmul(Am(:,:,i+1,j  ,plane),Dq(:,i+1,j  ,plane))&
+                                             +dscj(i  ,j+1,plane)*matmul(Bm(:,:,i  ,j+1,plane),Dq(:,i  ,j+1,plane)))
+
+            Dq(:,i,j,plane)=Dq(:,i,j,plane)+pre_sca(4,i,j,plane) *dot_product(pre2(:,i,j,plane),Dq(:,i,j,plane)) *pre1(:,i,j,plane)
+            Dq(:,i,j,plane)=1d0/pre_sca(1,i,j,plane)*Dq(:,i,j,plane)
+
+            res =res +Dq(nY+3,i,j,plane)**2
+            !do k=1,dimq
+            !   res =res +Dq(k,i,j,plane)**2
+            !end do
+
+            do k=1,nY
+               if(Dq(k,i,j,plane)<0d0) then
+                  omega = min(omega,-q(k,i,j,plane)/Dq(k,i,j,plane)*Dqmax)
+                  if(omega<omega_min) then
+                     print *,"Error: Omega becomes too small. omega = ",omega
+                     call exit(1)
+                  end if
+               end if
+            end do
+         end do
+         !$omp end parallel do
+      end do
    end do
 
    !!!!!!!!! FOR PARAMETERS ADJUSTMENTS !!!!!!!!!!!!!!!!
@@ -102,19 +105,21 @@ subroutine calc_next_step_implicit(step_internal)
          call exit(1)
       end if
       if(step_internal .eq. 1) res1 = res
-      write(66,'(i3,100e12.3)') step_internal,omega,res/res1,dt_grbl
+      if(ILwrite) write(66,'(i3,100e12.3)') step_internal,omega,res/res1,dt_grbl
    end if
    !!!!!!!!! END OF FOR PARAMETERS ADJUSTMENTS !!!!!!!!!
 
    omega      = min(omega_max,omega)
 
-   !add Dq
-   !$omp parallel do private(i)
-   do j=nys,nye
-      do i=nxs,nxe
-         q(:,i,j)=q(:,i,j)+omega*Dq(:,i,j)
+   do plane = nps,npe
+      !add Dq
+      !$omp parallel do private(i)
+      do j=nys(plane),nye(plane)
+         do i=nxs(plane),nxe(plane)
+            q(:,i,j,plane)=q(:,i,j,plane)+omega*Dq(:,i,j,plane)
+         end do
       end do
+      !$omp end parallel do
    end do
-   !$omp end parallel do
 end subroutine calc_next_step_implicit
 
