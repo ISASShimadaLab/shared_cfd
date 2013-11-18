df7920c774c90e4d382cb32f88288f12e746c039
shotayam <2921275202@mail.ecc.u-tokyo.ac.jp>
2013-11-18 13:37:35 +0900

    precon debug done

--- store/time_scheme/precon/time_precon_viscous.f90
+++ store/time_scheme/precon/time_precon_viscous.f90
@@ -7,20 +7,21 @@ subroutine calc_next_step_implicit(step_internal)
    integer,intent(in)::step_internal
    integer i,j,k,sm,plane
    double precision Dq(1:dimq,0:nimax+1,0:njmax+1,Nplane)
+   double precision Dq_small(1:dimq)
    double precision omega,tmp
 
    omega=1d300
    res = 0d0
    do plane = nps,npe
       !set RHS
-      !$omp parallel do private(i)
+      !$omp parallel do private(i,Dq_small)
       do j=nys(plane),nye(plane)
          do i=nxs(plane),nxe(plane)
-            Dq(:,i,j,plane)= dsi(i-1,  j,plane)*(TGi(1:dimq,i-1,j  ,plane)-TGvi(1:dimq,i-1,j  ,plane))&
-                            -dsi(  i,  j,plane)*(TGi(1:dimq,i  ,j  ,plane)-TGvi(1:dimq,i  ,j  ,plane))&
-                            +dsj(  i,j-1,plane)*(TGj(1:dimq,i  ,j-1,plane)-TGvj(1:dimq,i  ,j-1,plane))&
-                            -dsj(  i,  j,plane)*(TGj(1:dimq,i  ,j  ,plane)-TGvj(1:dimq,i  ,j  ,plane))
-            Dq(:,i,j,plane)= Dq(:,i,j,plane)+pre1(:,i,j,plane)*dot_product(pre2(:,i,j,plane),Dq(:,i,j,plane))
+            Dq_small= dsi(i-1,  j,plane)*(TGi(1:dimq,i-1,j  ,plane)-TGvi(1:dimq,i-1,j  ,plane))&
+                     -dsi(  i,  j,plane)*(TGi(1:dimq,i  ,j  ,plane)-TGvi(1:dimq,i  ,j  ,plane))&
+                     +dsj(  i,j-1,plane)*(TGj(1:dimq,i  ,j-1,plane)-TGvj(1:dimq,i  ,j-1,plane))&
+                     -dsj(  i,  j,plane)*(TGj(1:dimq,i  ,j  ,plane)-TGvj(1:dimq,i  ,j  ,plane))
+            Dq(:,i,j,plane)= Dq_small+pre1(:,i,j,plane)*dot_product(pre2(:,i,j,plane),Dq_small)
             Dq(:,i,j,plane)= Dq(:,i,j,plane)-Vol(i,j,plane)*dqdt(:,i,j,plane)
          end do
       end do
@@ -32,12 +33,12 @@ subroutine calc_next_step_implicit(step_internal)
 
       !calculate forward
       do sm=nxs(plane)+nys(plane),nxe(plane)+nye(plane)
-         !$omp parallel do private(j)
+         !$omp parallel do private(j,Dq_small)
          do i=max(nxs(plane),sm-nye(plane)),min(sm-nys(plane),nxe(plane))
             j=sm-i
-            Dq(:,i,j,plane)=alpha(i,j,plane)*(Dq(:,i,j,plane)&
-                                             +dsci(i-1,j  ,plane)*matmul(Ap(:,:,i-1,j  ,plane),Dq(:,i-1,j  ,plane))&
-                                             +dscj(i  ,j-1,plane)*matmul(Bp(:,:,i  ,j-1,plane),Dq(:,i  ,j-1,plane)))
+            Dq_small = dsci(i-1,j  ,plane)*matmul(Ap(:,:,i-1,j  ,plane),Dq(:,i-1,j  ,plane))&
+                      +dscj(i  ,j-1,plane)*matmul(Bp(:,:,i  ,j-1,plane),Dq(:,i  ,j-1,plane))
+            Dq(:,i,j,plane)=alpha(i,j,plane)*(Dq(:,i,j,plane)+Dq_small)
          end do
          !$omp end parallel do
       end do
