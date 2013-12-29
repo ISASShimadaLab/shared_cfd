c871c027a5e8c5ab47b7228f537a0b1dbf2f8040
shotayam <2921275202@mail.ecc.u-tokyo.ac.jp>
2013-12-29 18:23:37 +0900

    debug hp

--- store/therm_lib/chemkin/uvhp/hp/FJ.f90
+++ store/therm_lib/chemkin/uvhp/hp/FJ.f90
@@ -291,165 +291,29 @@ subroutine Fex(neq_outer, tt, n, dndt, rpar, ipar)!{{{
       end do
    end do
 
-   dh=0d0; cp=0d0
+   dh=0d0; cp=0d0; tmp=0d0
    do j=1,ns
-      dh=dh+vhrt(j)*dndt(j)
-      cp=cp+vcpr(j)*n(   j)
+      dh  =dh +vhrt(j)*dndt(j)
+      cp  =cp +vcpr(j)*n(   j)
+      tmp =tmp+        dndt(j)
    end do
    dndt(ns+1)=-dh*T/cp
+
+   !!! FOR hp problem rho*D(N) -> D(rho*N)!!!
+   tmp=tmp/sn+dndt(ns+1)/T
+   do j=1,ns
+      dndt(j)=-tmp*n(j)+dndt(j)
+   end do
 end subroutine Fex!}}}
-subroutine Fex_var(n, dndtp, dndtm)!{{{
+subroutine Fex_var(n, dndtp, dndtm)!dummy{{{
    !variables
    use const_chem
-   use chem
-   use func_therm
    implicit none
-   double precision,intent(in) :: n(ns+1)
-   double precision,intent(out):: dndtp(ns+1)
-   double precision,intent(out):: dndtm(ns+1)
+   double precision n(ns+1)
+   double precision dndtp(ns+1)
+   double precision dndtm(ns+1)
 
-   double precision,parameter::dd=0.14d0
-   double precision,parameter::loge210=0.43429448190325!log10(exp(1d0))
-
-   double precision T
-   double precision,dimension(ns)::vmu0rt,vhrt,vcpr
-   double precision vlogM,Dr,r
-   double precision vlogk(2)
-   double precision dh,cp,sn,logsn
-   double precision logT,Tinv
-   double precision logkinf,logk0
-   double precision Pr
-   double precision logFcent,logF,cc,nn,aa,log_Pr,logPrc,logPRT
-   double precision temp1,temp2,temp3,tmp
-   double precision,dimension(7)::cmurt,chrt,ccpr
-   integer          sec(ns)
-   integer i,j,k,itmp
-
-   T    =n(ns+1)
-   logT=log(T)
-   Tinv=1d0/T
-   logPRT=log(pst/Ru)-logT
-   sn   =sum(n(1:ns))
-   logsn=log(sn)
-
-   !set thermodynamical values
-   call check_section_number(T,sec)
-   call set_coeff(T,logT,cmurt,chrt,ccpr)
-   do j=1,ns
-      temp1=0d0
-      temp2=0d0
-      temp3=0d0
-      do i=1,7
-         tmp=coeff(i,sec(j),j)
-         temp1=temp1+tmp*cmurt(i)
-         temp2=temp2+tmp*chrt( i)
-         temp3=temp3+tmp*ccpr( i)
-      end do
-      vmu0rt(j)=temp1
-      vhrt(  j)=temp2
-      vcpr(  j)=temp3
-
-      dndtp(  j)=0d0
-      dndtm(  j)=0d0
-   end do
-
-   do i=1,nr
-      !calc M
-      if(exist_M(i)) then
-         tmp =sn
-         do j=1,NumM(i)
-            tmp=tmp+Men(j,i)*n(IndM(j,i))
-         end do
-
-         if(tmp .eq. sn) then
-            vlogM=logsn
-         else
-            vlogM=log(tmp)
-         end if
-      end if
-
-      !calc k
-      vlogk(1)=ABE(1,i)+ABE(2,i)*logT-ABE(3,i)*Tinv
-      vlogk(2)=0d0
-      if(Rstate(i,2) >= 4) vlogk(1)=vlogk(1)+vlogM
-
-      if(Rstate(i,1) .eq. 0) then !bi-direction
-         if(Rstate(i,2) .eq. 1 .or. Rstate(i,2) .eq. 5) then !rev
-            vlogk(2)=cABE(1,i)+cABE(2,i)*logT-cABE(3,i)*Tinv !read cABE
-            if(Rstate(i,2) .eq. 5) vlogk(2)=vlogk(2)+vlogM
-         else !none, low, troe
-            if(Rstate(i,2) .eq. 2 .or. Rstate(i,2) .eq. 3) then!low, troe
-               !Pr
-               logk0  =cABE(1,i)+cABE(2,i)*logT-cABE(3,i)*Tinv
-               logkinf=vlogk(1)
-               log_Pr=logk0-logkinf+vlogM
-               Pr=exp(log_Pr)
-               vlogk(1)=logkinf+log(Pr/(1d0+Pr))
-
-               !Troe
-               if(Rstate(i,2) .eq. 3) then
-                  aa=TROE(1,i)
-                  logFcent=log10((1d0-aa)*exp(-T   *TROE(2,i))&
-                                +aa      *exp(-T   *TROE(3,i))&
-                                +         exp(-Tinv*TROE(4,i)))
-                  cc=-0.4d0-0.67d0*logFcent
-                  nn=0.75d0-1.27d0*logFcent
-                  logPrc=log_Pr*loge210+cc
-                  logF=(1d0+(logPrc/(nn-dd*logPrc))**2)**(-1)*logFcent
-                  vlogk(1)=vlogk(1)+logF/loge210
-               end if
-            end if
-
-            !calc numu0rt
-            temp1=0d0
-            do k=1,NumNu(1,i)
-               temp1=temp1-vmu0rt(IndNu(k,1,i))
-            end do
-            do k=1,NumNu(2,i)
-               temp1=temp1+vmu0rt(IndNu(k,2,i))
-            end do
-
-            vlogk(2)=vlogk(1)-snu(i)*logPRT+temp1
-         end if
-      end if
-
-      !calc r
-      r=exp(vlogk(1))
-      do k=1,NumNu(1,i)
-         r=r*n(IndNu(k,1,i))
-      end do
-      do k=1,NumNu(1,i)
-         itmp=IndNu(k,1,i)
-         dndtm(itmp)=dndtm(itmp)-r
-      end do
-      do k=1,NumNu(2,i)
-         itmp=IndNu(k,2,i)
-         dndtp(itmp)=dndtp(itmp)+r
-      end do
-
-      if(Rstate(i,1) .eq. 0) then
-         r=exp(vlogk(2))
-         do k=1,NumNu(2,i)
-            r=r*n(IndNu(k,2,i))
-         end do
-         do k=1,NumNu(1,i)
-            itmp=IndNu(k,1,i)
-            dndtp(itmp)=dndtp(itmp)+r
-         end do
-         do k=1,NumNu(2,i)
-            itmp=IndNu(k,2,i)
-            dndtm(itmp)=dndtm(itmp)-r
-         end do
-      end if
-   end do
-
-   dh=0d0; cp=0d0
-   do j=1,ns
-      dh=dh+vhrt(j)*(dndtp(j)+dndtm(j))
-      cp=cp+vcpr(j)*n(   j)
-   end do
-   dndtp(ns+1)=max(-dh*T/cp,0d0)
-   dndtm(ns+1)=min(-dh*T/cp,0d0)
+   !!!!!!!!!!!!!!!!!!!! dummy !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 end subroutine Fex_var!}}}
 subroutine Jex(neq_outer, tt, n, ML, MU, dndn, nrpd, rpar, ipar)!{{{
    !variables
@@ -697,13 +561,16 @@ subroutine Jex(neq_outer, tt, n, ML, MU, dndn, nrpd, rpar, ipar)!{{{
       dndn(ns+1,j)=-(T*temp1+dndt(ns+1)*vcpr(j))/cp
    end do
 
-   !do j=1,ns+1
-   !   do i=1,ns+1
-   !      if(isNaN(dndn(i,j))) then
-   !         print *,i,j,"NaN!"
-   !         call exit(1)
-   !      end if
-   !   end do
-   !end do
+   !!! FOR hp problem rho*D(N) -> D(rho*N)!!!
+   do j=1,ns+1
+      tmp=0d0
+      do i=1,ns
+         tmp=tmp+dndn(i,j)
+      end do
+      tmp=tmp/sn+dndn(ns+1,j)/T
+      do i=1,ns
+         dndn(i,j)=-tmp*n(i)+dndn(i,j)
+      end do
+   end do
 end subroutine Jex!}}}
 
