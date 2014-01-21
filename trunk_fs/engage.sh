8aa561cb3c709f876076b68cdf155d46a39b0890
Shota Yamanaka <shoooterp@yahoo.co.jp>
2014-01-21 22:45:28 +0900

    update

--- trunk_fs/engage.sh
+++ trunk_fs/engage.sh
@@ -2,3 +2,16 @@ cp trunk_fs/control.inp       checkout/
 cp trunk_fs/control_chem.inp  checkout/
 cp trunk_fs/condition.f90     checkout/
 cp trunk_fs/restart.bin       checkout/
+rm -f  checkout/*raw*
+if [ -e checkout/mod_mpi.f90 ]; then
+   cp -r checkout share_code
+   tar czvf share_code.tar.gz share_code
+   rm -rf share_code
+   echo "y535 or o384 ('y' or 'o'?)"
+   read var
+   if [ "${var}" = "o" ]; then
+      scp share_code.tar.gz majao:/large/o/o384/
+   else
+      scp share_code.tar.gz maja:/large/y/y535/2dcfd/
+   fi
+fi
